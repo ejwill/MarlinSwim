dc0b5f0b40ac8e451f78cf01ccd8c2ada2f8ae19
ejwill <jwill.will@gmail.com>
2013-06-07 21:22:00 -0400

    Push Notifications, Splash screen, app icon, etc
    Push Notifications (Pushwoosh) were corrected.  If you plan on using
    phonegap and not phonegap build choose pushwoosh as the setting for your
    app not phonegap build (generic).  Also use pages as it will create a
    page that pops up when the push is sent.  Splash screen and app icon
    were added.  Use makeappicon.com to quickly make app icons of the
    correct sizes.  Everything else is pretty simple.

--- src/ejw/MarlinSwim/MyPhoneGapActivity.java
+++ src/ejw/MarlinSwim/MyPhoneGapActivity.java
@@ -1,5 +1,6 @@
 package ejw.MarlinSwim;
 
+import org.apache.cordova.Config;
 import org.apache.cordova.DroidGap;
 import android.os.Bundle;
 
@@ -7,6 +8,8 @@ public class MyPhoneGapActivity extends DroidGap {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		super.setIntegerProperty("splashscreen", R.drawable.spashscreen);
+        super.loadUrl(Config.getStartUrl(), 9500);
 		super.loadUrl("file:///android_asset/www/index.html");
 	}
 }
