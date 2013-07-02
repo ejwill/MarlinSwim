52e720c9edc21f98432b027efff4ec168c450bde
Erwin James Will <jwill.will@gmail.com>
2013-07-02 17:08:32 -0400

    update

--- iOS/granite_swim_team/www/js/index.js
+++ iOS/granite_swim_team/www/js/index.js
@@ -16,34 +16,70 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+function initPushwoosh() {
+	var pushNotification = window.plugins.pushNotification;
+	pushNotification.onDeviceReady();
+	
+	pushNotification.registerDevice({alert:true, badge:true, sound:true, pw_appid:"7F7E9-06915", appname:"Marlin_Swim_Team"},
+									function(status) {
+                                    var deviceToken = status['deviceToken'];
+                                    console.warn('registerDevice: ' + deviceToken);
+									},
+									function(status) {
+                                    console.warn('failed to register : ' + JSON.stringify(status));
+                                    navigator.notification.alert(JSON.stringify(['failed to register ', status]));
+									});
+	
+	pushNotification.setApplicationIconBadgeNumber(0);
+    
+	//start geo tracking. PWTrackSignificantLocationChanges - Uses GPS in foreground, Cell Triangulation in background.
+	pushNotification.startLocationTracking('PWTrackSignificantLocationChanges',
+                                           function() {
+										   console.warn('Location Tracking Started');
+                                           });
+	
+	document.addEventListener('push-notification', function(event) {
+                              var notification = event.notification;
+                              
+                              navigator.notification.alert(notification.aps.alert);
+                              
+                              //to view full push payload
+                              //navigator.notification.alert(JSON.stringify(notification));
+                              
+                              pushNotification.setApplicationIconBadgeNumber(0);
+							  });
+}
+
 var app = {
     // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
+initialize: function() {
+    this.bindEvents();
+},
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+},
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
+onDeviceReady: function() {
+    initPushwoosh();
+    app.receivedEvent('deviceready');
+},
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+receivedEvent: function(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+    
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+    
+    console.log('Received Event: ' + id);
+}
 };
